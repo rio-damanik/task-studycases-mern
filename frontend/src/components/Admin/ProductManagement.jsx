0f3a2468efd1d4fe8f762f23ce8b53952c4f879e
rio-damanik <rio71385@gmail.com>
2025-03-15 12:04:26 +0700

    update new

--- frontend/src/components/Admin/ProductManagement.jsx
+++ frontend/src/components/Admin/ProductManagement.jsx
@@ -415,12 +415,14 @@ const ProductManagement = () => {
                       {isValidUrl(imagePreview) ?
                         <img
                           src={imagePreview || '/placeholder.png'}
+                          alt="Product preview"
                           onError={(e) => {
                             e.target.onerror = null;
                             e.target.src = '/placeholder.png';
                           }}
                         /> : <img
                           src={'http://localhost:8000/uploads/' + imagePreview}
+                          alt="Product preview"
                         onError={(e) => {
                           e.target.onerror = null;
                           e.target.src = '/placeholder.png';
